0a1a098aac7035aa2597ba6385f2013ec5233a03
John Goerzen <jgoerzen@complete.org>
2010-08-24 01:04:02 -0500

    Initial go at Hamlib.java

--- com/loukides/jl/transceivers/Hamlib.java
+++ com/loukides/jl/transceivers/Hamlib.java
@@ -11,67 +11,78 @@ import java.text.DecimalFormat;
 import java.text.FieldPosition;
 
 /**
-A class to implement Elecraft K2 rig control for jl
+A class to implement Hamlib rig control for jl
 */
-public class ElecraftK2 extends AbstractTransceiver {
+public class Hamlib extends AbstractTransceiver {
 
   private InputStream is;
   private OutputStream os;
   private DecimalFormat frequencyFormat = new DecimalFormat("00000000000");
   private DecimalFormat memoryFormat = new DecimalFormat("00");
   private FieldPosition pos = new FieldPosition(NumberFormat.INTEGER_FIELD);
-  private String portName = "";
-  private int timeout = 30;
-  private int baudrate = 4800;
+  private String commandName = "";
+  private String commandArgs[] = [];
 
-
-  public ElecraftK2() {}
+  public Hamlib() {}
 
   public void setProperties(Properties p) throws Exception {
      super.setProperties(p);
-     portName = p.getProperty("transceiver.port","/dev/ttyS0");
-     baudrate = Integer.parseInt(p.getProperty("transceiver.baudrate","4800"));
-     timeout = Integer.parseInt(p.getProperty("transceiver.timeout", "30"));
-     SerialPort serialPort =
-        (SerialPort)SerialPortFactory.getSerialPort(portName,4800,30);
-     is = serialPort.getInputStream();
-     os = serialPort.getOutputStream();
-     try {
-        os.write("k22;".getBytes()); //K2 Extended mode
-     } catch (Exception e) {e.printStackTrace(); }
+     commandName = p.getProperty("transceiver.command", "rigctl -m 122 -r /dev/ttyUSB0 -s 4800");
+     commandArgs = commandName.split(" \t\n\r\f");
+  }
+
+  private String runCommand(List<String> args) throws Exception {
+    List commands = Arrays.asList(commandArgs);
+    commands.addAll(args);
+    ProcessBuilder pb = new ProcessBuilder(commands);
+    Process proc = pb.start();
+    InputStream is = proc.getInputStream();
+    String line;
+    StringBuilder sb = new StringBuilder();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+    if ((line = reader.readLine()) != null) {
+        sb.append(line);
+    }
+    /*
+    while ((line = reader.readLine()) != null) {
+        sb.append(line).append("\n");
+    }
+    */
+    is.close();
+    int exitcode = proc.waitFor();
+    if (exitcode != 0) {
+        throw new Exception("Nonzero exit code from command");
+    }
+    return sb.toString();
   }
 
 /**
-Store the current state of the K2 in one of its memories.
-@param mem The K2 memory in which to store the current state. Only values
-0 to 9 are legal.
+Store the current state of the rig in one of its memories.
 */
    public void storeMemory(int mem) {
-      StringBuffer memoryCommand = new StringBuffer("sw19;sw");
-      memoryFormat.format(mem+7,memoryCommand,pos);
-      memoryCommand.append(";");
+      List args = new ArrayList<String>;
+      args.add("set_mem");
+      args.add(Integer.toString(mem));
+
       try {
-         os.write(memoryCommand.toString().getBytes());
-         os.flush();
+         runCommand(args);
       } catch (Exception ee) {
          ee.printStackTrace();
       }
    }
 
 /**
-Recalls the current state of the K2 from one of its memories.
-Note this will give different behavior than the jl docs, since, for
+Recalls the current state of the rig from one of its memories.
+Note this may? different behavior than the jl docs, since, for
 example, it will remember if the rig is in split, etc.
-@param mem The K2 memory from which to recall the state. Only values
-0 to 9 are legal.
 */
    public void setVFOFromMemory(int mem) {
-      StringBuffer memoryCommand = new StringBuffer("sw17;sw");
-      memoryFormat.format(mem+7,memoryCommand,pos);
-      memoryCommand.append(";");
+      List args = newArrayList<String>;
+      args.add("get_mem");
+      args.add(Integer.toString(mem));
+      
       try {
-         os.write(memoryCommand.toString().getBytes());
-         os.flush();
+         runCommand(args);
       } catch (Exception ee) {
          ee.printStackTrace();
       }
@@ -81,7 +92,10 @@ example, it will remember if the rig is in split, etc.
 Set transmit frequency for split operation.
 @param f the transmit frequency in KHz. If zero cancel split.
 */
+/*
    public void setSplit(float f) {
+      List args = new ArrayList<String>;
+      args.add("
       StringBuffer frequencyCommand;
       int irx = getRxVFO();
       if (f == 0.0) {
@@ -103,25 +117,18 @@ Set transmit frequency for split operation.
          ee.printStackTrace();
       }
    }
-
+*/
 /**
 Cancel split and set the current receive VFO to f.
 @param f The frequency in KHz.
 */
    public void setFrequency(float f) {
-      StringBuffer frequencyCommand;
-      int irx = getRxVFO();
-      setRxVFO(irx); //cancels split
-      if (irx == 0) {
-         frequencyCommand = new StringBuffer("fa");
-      } else {
-         frequencyCommand = new StringBuffer("fb");
-      }
-      frequencyFormat.format((long) (f*1000.0f),frequencyCommand,pos);
-      frequencyCommand.append(";");
+      long freq = (long) (f * 1000.0f);
+      List args = newArrayList<String>;
+      args.add("set_freq");
+      args.add(Long.toString(freq));
       try {
-         os.write(frequencyCommand.toString().getBytes());
-         os.flush();
+         runCommand(args);
       } catch (Exception ee) {
          ee.printStackTrace();
       }
@@ -130,6 +137,7 @@ Cancel split and set the current receive VFO to f.
 /**
 Return 0 if the current receive VFO is A, 1 if B
 */
+/*
    public int getRxVFO() {
       byte[] b = new byte[4];
       try {
@@ -144,11 +152,12 @@ Return 0 if the current receive VFO is A, 1 if B
       }
       return (int) (b[2]-0x30); //0 for VFO-A, 1 for VFO-B
    }
-
+*/
 /**
 Set the current receive VFO, cancelling any split operation.
 @param vfo 0 for VFO A, 1 for VFO B
 */
+/*
    public void setRxVFO(int vfo) { //cancels split
       byte[] b = new byte[4];
       b = "frX;".getBytes();
@@ -161,34 +170,29 @@ Set the current receive VFO, cancelling any split operation.
          ee.printStackTrace();
       }
    }
-
+*/
 /**
 Get the frequency in KHz of the current receive VFO.
 */
    public float getFrequency() {
-      byte[] b = new byte[14];
-      int irx = getRxVFO(); //get current vfo (also cancels split)
+      List args = new ArrayList<String>;
+      args.add("get_freq");
       try {
-         while (is.available() > 0) is.read();
-         if (irx == 0) {
-            os.write("fa;".getBytes());
-         } else {
-            os.write("fb;".getBytes());
-         }
-         os.flush();
-         for (int i=0;i<14;i++) {
-            b[i] = (byte) is.read();
-         }
+         String result = runCommand(args);
+         Long l = Long.parseLong(result.trim());
+         return ((l.floatValue()) / 1000.0f);
       } catch (Exception ee) {
          ee.printStackTrace();
       }
-      return new Float(new String(b,2,11)).floatValue()*0.001f;
   } 
 
 /**
 Get the Mode. The possible returned values are CW, PH, or RY.
 */
+/*
   public String getMode() {
+      List args = new ArrayList<String>;
+      
       byte[] b = new byte[4];
       try {
          while (is.available() > 0) is.read();
@@ -217,3 +221,4 @@ Get the Mode. The possible returned values are CW, PH, or RY.
    } 
 
 }
+*/
